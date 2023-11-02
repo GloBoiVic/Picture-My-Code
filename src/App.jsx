8fb0502d61a860a25b31f2029b2f8721008dd788
GloBoiVic <victorike08@gmail.com>
2023-11-02 11:02:32 -0500

    Update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useRef } from 'react';
+import CodeEditor from './CodeEditor';
+import { cn } from './lib/utils';
+import { fonts, themes } from './data/options';
+import useCodeStore from './store';
+
 function App() {
-	return <h1>Hello World</h1>;
+	const theme = useCodeStore((state) => state.theme);
+	const padding = useCodeStore((state) => state.padding);
+	const fontStyle = useCodeStore((state) => state.fontStyle);
+	const showBackground = useCodeStore((state) => state.showBackground);
+
+	const editorRef = useRef(null);
+
+	useEffect(() => {
+		const queryParams = new URLSearchParams(location.search);
+		if (queryParams.size === 0) return;
+
+		const state = Object.fromEntries(queryParams);
+
+		useCodeStore.setState({
+			...state,
+			code: state.code ? atob(state.code) : '',
+			// theme: state.theme ? state.theme : 'oceanic',
+			autoDetectLanguage: state.autoDetectLanguage === 'true',
+			darkMode: state.darkMode === 'true',
+			fontSize: Number(state.fontSize || 18),
+			padding: Number(state.padding || 64),
+		});
+	}, []);
+
+	return (
+		<main className='flex items-center justify-center min-h-screen text-white dark bg-neutral-950'>
+			<link rel='stylesheet' href={themes[theme].theme} crossOrigin='anonymous' />
+			<link rel='stylesheet' href={fonts[fontStyle].src} crossOrigin='anonymous' />
+			<div
+				className={cn(
+					'overflow-hidden mb-2 transition-all ease-out',
+					showBackground ? themes[theme].background : 'ring ring-neutral-900'
+				)}
+				style={{ padding }}
+				ref={editorRef}
+			>
+				<CodeEditor />
+			</div>
+		</main>
+	);
 }
 
 export default App;
---
+++ src/CodeEditor.jsx
@@ -0,0 +1,74 @@
+import hljs from 'highlight.js';
+import Editor from 'react-simple-code-editor';
+import { cn } from './lib/utils';
+import { codeSnippets, fonts } from './data/options';
+
+import useCodeStore from './store';
+import { useEffect } from 'react';
+import flourite from 'flourite';
+
+function CodeEditor() {
+	const store = useCodeStore();
+
+	useEffect(() => {
+		const randomSnippet = codeSnippets[Math.floor(Math.random() * codeSnippets.length)];
+		useCodeStore.setState(randomSnippet);
+	}, []);
+
+	useEffect(() => {
+		if (store.autoDetectLanguage) {
+			const { language } = flourite(store.code, { noUnknown: true });
+			useCodeStore.setState({
+				language: language.toLowerCase() || 'plaintext',
+			});
+		}
+	}, [store.autoDetectLanguage, store.code]);
+	return (
+		<div
+			className={cn(
+				'min-w-[400px] border-2 rounded-xl shadow-2xl',
+				store.darkMode ? 'bg-black/75 border-gray-600/40' : 'bg-white/75 border-gray-200/20'
+			)}
+			onClick={() => useCodeStore.setState({ darkMode: !store.darkMode })}
+		>
+			<header className='grid items-center grid-cols-6 gap-3 px-4 py-3'>
+				<div className='flex gap-1.5'>
+					<div className='w-3 h-3 bg-red-500 rounded-full'></div>
+					<div className='w-3 h-3 bg-yellow-500 rounded-full'></div>
+					<div className='w-3 h-3 bg-green-500 rounded-full'></div>
+				</div>
+				<div className='flex justify-center col-span-4'>
+					<input
+						type='text'
+						value={store.title}
+						onChange={(e) => useCodeStore.setState({ title: e.target.value })}
+						spellCheck={false}
+						onClick={(e) => e.target.select()}
+						className='text-sm font-medium text-center text-gray-400 bg-transparent focus:outline-none'
+					/>
+				</div>
+			</header>
+			<div
+				className={cn(
+					'px-4 pb-4',
+					store.darkMode
+						? 'brightness-110'
+						: 'text-gray-800 brightness-50 saturate-200 contrast-200'
+				)}
+			>
+				<Editor
+					value={store.code}
+					onValueChange={(code) => useCodeStore.setState({ code })}
+					highlight={(code) =>
+						hljs.highlight(code, { language: store.language || 'plaintext' }).value
+					}
+					style={{ fontFamily: fonts[store.fontStyle].name, fontSize: store.fontSize }}
+					textareaClassName='focus:ouline-none'
+					onClick={(e) => e.target.select()}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default CodeEditor;
